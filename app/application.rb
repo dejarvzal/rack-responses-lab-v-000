afac4a896eb831d8ce127475867c79ec2957a360
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 03:50:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,11 +9,11 @@ class Application
     time_2 = Kernel.rand(12..24)
   #   num_3 = Kernel.rand(12..20)
   #
-    resp.write "#{time_1}\n"
-    resp.write "#{time_2}\n"
+    # resp.write "#{time_1}\n"
+    # resp.write "#{time_2}\n"
   #   resp.write "#{num_3}\n"
   #
-    if time_1<num_2 && num_2==num_3
+    if time_1 < 12 && num_2==num_3
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"

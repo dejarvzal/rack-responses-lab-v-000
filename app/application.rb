7cf81764f57f78650ff4f549f26fccd56c129623
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 03:53:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,13 +6,7 @@ class Application
     resp.finish
   #
     time_1 = Kernel.rand(0..24)
-    # time_2 = Kernel.rand(12..24)
-  #   num_3 = Kernel.rand(12..20)
-  #
-    # resp.write "#{time_1}\n"
-    # resp.write "#{time_2}\n"
-  #   resp.write "#{num_3}\n"
-  #
+    
     if time_1 < 12
       resp.write "Good Morning"
     else

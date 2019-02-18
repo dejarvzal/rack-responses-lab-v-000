6a14295e9963b816702b5c45a398b2faf803c8ae
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 03:59:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     resp.write "Good Morning!"
     resp.finish
   #
-    time_1 = Kernel.rand(0..24)
+    time_now = Kernel.rand(0..24)
 
     if time_1 < 12
       resp.write "Good Morning"
